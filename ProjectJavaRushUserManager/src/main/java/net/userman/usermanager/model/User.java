ba2b5fc6844bbe6c6f94f55bfcbe542386665a32
AIVENGO\ASUS <alexobukhovarno@gmail.com>
2017-11-28 18:45:05 +0300

    ProjectJavaRushUserManager

---
+++ ProjectJavaRushUserManager/src/main/java/net/userman/usermanager/model/User.java
@@ -0,0 +1,63 @@
+package net.userman.usermanager.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "USER_NAME")
+    private String userName;
+
+    @Column(name = "USER_AGE")
+    private String userAge;
+
+    @Column(name = "USER_SEX")
+    private int sex;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(String userAge) {
+        this.userAge = userAge;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", userAge='" + userAge + '\'' +
+                ", sex=" + sex +
+                '}';
+    }
+}
